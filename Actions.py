15797d617868926bf2b58061e22cbc5783c3faae
jasmi <brubpool9@gmail.com>
2025-03-27 21:00:34 -0400

    testing and sutff.

--- Actions.py
+++ Actions.py
@@ -1,10 +1,3 @@
-from typing import Union
-from p import Player
-from Entities import Entity
-class  Actions:
-    def hit(self, target: Union[Player, Entity] , damage: int): 
-        pass
-    
-    
+
 
     
--- test.py
+++ test.py
@@ -1,1 +1,37 @@
-import Player
+from Map import map
+from Player import player
+from os import system
+player = player([0,0])
+m = map(10, 5, cgen=True, mplayer=player)
+m.loadChunk(-1, 0)
+'''
+m.loadChunk(1, 0)
+m.loadChunk(2, 0)
+m.loadChunk(3, 0)
+m.loadChunk(3, -1)
+m.loadChunk(3, 1)
+'''
+
+done = 0
+while True:
+
+    system('cls')
+    m.displaysolidAll()
+    done += 1
+    #print("done: ", done)
+    print("pos: ", player.pos)
+    try:
+        print(m.tiledata[(player.pos[0], player.pos[1])])
+    except KeyError:
+        pass
+    a = input('')
+    if a == 'w':
+        player.move('up')
+    elif a == 's':
+        player.move('down')
+    elif a == 'a':
+        player.move('left')
+    elif a == 'd':
+        player.move('right')
+
+    player.updateAll()
