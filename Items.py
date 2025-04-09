aa0e07709c990ce8f5e73eb49715002d79e8e901
brubdope9 <100808067+brubdope9@users.noreply.github.com>
2025-04-09 22:42:27 +0000

    Carrot.

--- Items.py
+++ Items.py
@@ -2,4 +2,5 @@ items = {'sword': {'name': 'sword', 'damage': 10, 'durability': 100, 'type': 'we
          'shield': {'name': 'shield', 'defense': 5, 'durability': 100, 'type': 'armor', 'equippable': True, 'slot': 'shield'},
          'potion': {'name': 'potion', 'heal': 20, 'type': 'consumable', 'equippable': False},
          'gold': {'name': 'gold', 'value': 1, 'type': 'currency', 'equippable': False},
+         'carrot': {'name': 'carrot', 'heal': 5, 'type': 'food', 'equippable': False},
         }
